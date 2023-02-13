26cfd93028df95900614a880d97220aea21c8c21
MarioMolina23 <94089210+MarioMolina23@users.noreply.github.com>
2023-02-13 00:09:01 -0600

    Plantilla adaptada al proyecto

--- TiendaMascotas/TiendaMascotas/Controllers/HomeController.cs
+++ TiendaMascotas/TiendaMascotas/Controllers/HomeController.cs
@@ -10,21 +10,15 @@ namespace TiendaMascotas.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult components()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
